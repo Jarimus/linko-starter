4e7ef8ab6ec10f3526c7327a2b98c2c32b50f477
Jari Ahonen <jari.n.ahonen@gmail.com>
2026-08-10 22:07:08 +0300

    Chapter 4 lesson 4

--- auth.go
+++ auth.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log/slog"
 	"net/http"
 
 	"golang.org/x/crypto/bcrypt"
@@ -34,7 +34,11 @@ func (s *server) authMiddleware(next http.Handler) http.Handler {
 		}
 		ok, err := s.validatePassword(password, stored)
 		if err != nil {
-			s.logger.Info(fmt.Sprintf("error validating password for user: %s, error: %v\n", username, err))
+			s.logger.Info(
+				"error validating password",
+				slog.String("user", username),
+				slog.String("error", err.Error()),
+			)
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			return
 		}
@@ -53,7 +57,6 @@ func (s *server) validatePassword(password, stored string) (bool, error) {
 		return false, nil
 	}
 	if err != nil {
-		s.logger.Error(fmt.Sprintf("error validating password: %v\n", err))
 		return false, err
 	}
 	return true, nil
